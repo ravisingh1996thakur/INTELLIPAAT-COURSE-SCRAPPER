269dfe509bca76f5830477be0430ebb2a26d063f
RAVI SINGH <ravisingh@RAVIs-MacBook-Air.local>
2023-03-31 16:28:08 +0530

    initial commit

--- application.py
+++ application.py
@@ -37,5 +37,5 @@ def download(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5002)
 
--- s3Utils.py
+++ s3Utils.py
@@ -5,14 +5,14 @@ from botocore.exceptions import ClientError
 class S3Utils:
     def __init__(self):
         self.access_key = 'AKIATR3L5NH5BDT6UJNC'
-        self.secret = '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'
+        self.secret = 'S3_SECRET'
         self.bucket_name = 'ineuron-courses-bucket'
         self.s3_client = boto3.client(
             's3',
             aws_access_key_id=self.access_key,
             aws_secret_access_key=self.secret
         )
-    def upload_file(self,file_path):
+    def upload_file(self, file_path):
         try:
             file_name=file_path.split("/")[-1]
             self.s3_client.upload_file(
